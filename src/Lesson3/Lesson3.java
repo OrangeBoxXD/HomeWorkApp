a7a386123875acc32c50291024fb08afd6bb44c5
OrangeBoxXD <84081553+OrangeBoxXD@users.noreply.github.com>
2021-05-25 13:16:35 +0300

    minor architecture and bugs fix

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -6,42 +6,63 @@ import java.util.Scanner;
 
 public class Lesson3 {
     public static void main(String[] args) {
-    //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-    // С помощью цикла и условия заменить 0 на 1, 1 на 0;
-        IntegerArray();
+        System.out.println("Enter task number: ");
+        Scanner taskNumber = new Scanner(System.in);
+        int whichTask = taskNumber.nextInt();
+        switch(whichTask) {
+            //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+            // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+            case 1:
+                IntegerArray();
+                break;
 
-    //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100
-        System.out.println(HundredLenghtArray());
+            //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100
+            case 2:
+                System.out.println(Arrays.toString(HundredLenghtArray()));
+                break;
 
-        //3. Задать массив [  ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
-        DoubleIfLess5();
+            //3. Задать массив [  ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+            case 3:
+                DoubleIfLess5();
+                break;
 
-    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
-    // и с помощью цикла(-ов) заполнить его диагональные элементы единицами
-    // (можно только одну из диагоналей, если обе сложно).
-    // Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
-    // то есть [0][0], [1][1], [2][2], …, [n][n];
-        SquareArray();
+            //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+            // и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+            // (можно только одну из диагоналей, если обе сложно).
+            // Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
+            // то есть [0][0], [1][1], [2][2], …, [n][n];
+            case 4:
+                SquareArray();
+                break;
 
-    //5. Написать метод, принимающий на вход два аргумента: len и initialValue,
-    // и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
-        System.out.println(Arrays.toString(MakeAnArray()));
+            //5. Написать метод, принимающий на вход два аргумента: len и initialValue,
+            // и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
+            case 5:
+                System.out.println(Arrays.toString(MakeAnArray()));
+                break;
 
-    //6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
-        System.out.println(Arrays.toString(ExtremesOfArray(10)));
+            //6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+            case 6:
+                System.out.println(Arrays.toString(ExtremesOfArray(10)));
+                break;
 
-    //7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
-    // метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-        int[] checkArray = {2, 2, 2, 1, 2, 2, 10, 1};
-        System.out.println(EqualSumsOfArray(checkArray));
+            //7. ** Написать метод, в который передается не пустой одномерный целочисленный массив,
+            // метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+            case 7:
+                int[] checkArray = {2, 2, 2, 1, 2, 2, 10, 1};
+                System.out.println(EqualSumsOfArray(checkArray));
+                break;
 
-    //8. *** Написать метод, которому на вход подается одномерный массив и число n
-    // (может быть положительным, или отрицательным), при этом метод должен сместить все элементы массива на n позиций.
-    // Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
-    // Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ].
-    // При каком n в какую сторону сдвиг можете выбирать сами.
-        System.out.println("Negative moves left, positive moves right");
-        System.out.println(Arrays.toString(MoveHowMuch(5, ILikeToMoveItMoveIt(HundredLenghtArray()))));;
+            //8. *** Написать метод, которому на вход подается одномерный массив и число n
+            // (может быть положительным, или отрицательным), при этом метод должен сместить все элементы массива на n позиций.
+            // Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
+            // Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ].
+            // При каком n в какую сторону сдвиг можете выбирать сами
+            case 8:
+                System.out.println("Negative moves left, positive moves right");
+            System.out.println(Arrays.toString(MoveHowMuch(-10, ILikeToMoveItMoveIt(HundredLenghtArray()))));
+            break;
+        }
     }
     static void IntegerArray() {
         System.out.println("Enter string lenght: ");
@@ -73,13 +94,13 @@ public class Lesson3 {
     static void DoubleIfLess5() {
         System.out.println("This is you want me to do: ");
         int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-//        System.out.println(array.length);
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 6) array[i] *= 2;
         }
         System.out.println(Arrays.toString(array));
 
         System.out.println("This is more random: ");
+        System.out.println("Enter lenght of new array:");
         Scanner stringLenght = new Scanner(System.in);
         Random arrayRandom = new Random();
         int lenght2 = stringLenght.nextInt();
@@ -112,6 +133,7 @@ public class Lesson3 {
         }
     }
     static String[] MakeAnArray() {
+        System.out.println("Enter len and initialValue separated by space");
         Scanner mkArray = new Scanner(System.in);
         int len = mkArray.nextInt();
         String initialValue = mkArray.nextLine();    //работает, но ввод происходит в одну строку, надо узнать, можно ли
